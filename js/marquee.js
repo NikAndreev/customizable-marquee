70cc939b063e063c95b550d492086d34985b800e
Nikita Andreev <andreev.nikita@raketa.agency>
2022-07-01 18:51:44 +0300

    divided into methods

--- js/marquee.js
+++ js/marquee.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function(){
       this._clones = [this._content]
       this._shift = config.shift
       this._interval = config.interval
-      this._scroll = 0
+      this._scrollX = 0
       this._offset = 0
       
       this._init()
@@ -16,23 +16,33 @@ document.addEventListener('DOMContentLoaded', function(){
 
     _init() {
       setInterval(() => {
-        this._scroll += this._shift
-        this._track.style.transform = 'translateX(-' + this._scroll + 'px)'
-        
-        if (this._content.getBoundingClientRect().right <= this._marquee.getBoundingClientRect().right) {
-          const clone = this._content.cloneNode(true)
-          this._track.appendChild(clone)
-          this._clones.push(clone)
-          this._content = clone
-        }
-
-        if (this._clones.length > 0 && this._clones[0].getBoundingClientRect().left + this._clones[0].offsetWidth <= this._marquee.getBoundingClientRect().left) {
-          this._offset += this._clones[0].offsetWidth
-          this._track.style.paddingLeft = `${this._offset}px`
-          this._clones[0].remove()
-          this._clones.splice(0, 1)
-        }
+        this._scroll()
+        this._add()
+        this._remove()
       }, this._interval)
+    }
+
+    _scroll() {
+      this._scrollX += this._shift
+      this._track.style.transform = 'translateX(-' + this._scrollX + 'px)'
+    }
+
+    _add() {
+      if (this._content.getBoundingClientRect().right <= this._marquee.getBoundingClientRect().right) {
+        const clone = this._content.cloneNode(true)
+        this._track.appendChild(clone)
+        this._clones.push(clone)
+        this._content = clone
+      }
+    }
+
+    _remove() {
+      if (this._clones.length > 0 && this._clones[0].getBoundingClientRect().left + this._clones[0].offsetWidth <= this._marquee.getBoundingClientRect().left) {
+        this._offset += this._clones[0].offsetWidth
+        this._track.style.paddingLeft = `${this._offset}px`
+        this._clones[0].remove()
+        this._clones.splice(0, 1)
+      }
     }
   }
 
