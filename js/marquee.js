c470cc2e9e7147956094ee79237bccceacdeadc8
Nikita <nik.andreev.99@yandex.ru>
2022-07-02 13:40:49 +0300

    improved js

--- js/marquee.js
+++ js/marquee.js
@@ -28,16 +28,15 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     _add() {
-      if (this._content.getBoundingClientRect().right <= this._marquee.getBoundingClientRect().right) {
+      if (this._clones[this._clones.length - 1].getBoundingClientRect().right - this._shift * 2 <= this._marquee.getBoundingClientRect().right) {
         const clone = this._content.cloneNode(true)
         this._track.appendChild(clone)
         this._clones.push(clone)
-        this._content = clone
       }
     }
 
     _remove() {
-      if (this._clones.length > 0 && this._clones[0].getBoundingClientRect().left + this._clones[0].offsetWidth <= this._marquee.getBoundingClientRect().left) {
+      if (this._clones[0].getBoundingClientRect().left + this._clones[0].offsetWidth <= this._marquee.getBoundingClientRect().left) {
         this._offset += this._clones[0].offsetWidth
         this._track.style.paddingLeft = `${this._offset}px`
         this._clones[0].remove()
